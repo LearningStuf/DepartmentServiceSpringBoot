dc29b0b2e0cf43dc93ffe772f5e1a275c813171f
Hassan Haroon <hassanharoon022@gmail.com>
2021-11-08 17:41:39 +0500

    RestFull Webservice created on spring boot

---
+++ src/main/java/com/tutorial/department/service/entity/Department.java
@@ -0,0 +1,24 @@
+package com.tutorial.department.service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "department")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+}
